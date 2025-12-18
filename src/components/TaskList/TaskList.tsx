3b79f1b6627aaf874781ab04fa0252d5538c964f
kostyaslob <kostyaslob@gmail.com>
2025-12-18 21:38:25 +0200

    feat: create store with initialState

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -2,14 +2,6 @@ import css from "./TaskList.module.css";
 import Task from "./Task/Task"
 
 export default function TaskList() {
-  const tasks = [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ];
-
   return (
     <ul className={css.list}>
       {tasks.map((task) => (
