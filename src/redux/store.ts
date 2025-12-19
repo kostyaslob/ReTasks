59bf7fc36a064aef3debd971cedef0bfb2b6a95b
kostyaslob <kostyaslob@gmail.com>
2025-12-19 19:31:34 +0200

    feat: add reducers to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -26,9 +26,51 @@ const initialState: RootState = {
 };
 
 
-const rootReducer = (state: RootState = initialState, action: {type: string}): RootState => {
-  return state;
-};
+const rootReducer = (state: RootState = initialState, action: { type: string }): RootState => {
+  switch (action.type) {
+    case "tasks/addTask": {
+      return {
+        ...state,
+        tasks: {
+          items: [...state.tasks.items, action.payload]
+        }
+      }
+    }
+    case "tasks/deleteTask": {
+      return {
+        ...state,
+        tasks: {
+          items: state.tasks.items.filter((task) => task.id !== action.payload)
+        }
+      }
+    }
+    case "tasks/toggleCompleted": {
+      return {
+        ...state,
+        tasks: {
+          items: state.tasks.items.map((task) => {
+            if (task.id !== action.payload) {
+              return task
+            }
+            return {
+              ...task,
+              completed: !task.completed
+            }
+          })
+        }
+      }
+    }
+      case "filters/setStatusFilter": 
+      return {
+        ...state,
+        filters: {
+          status: action.payload,
+        }
+      }      
+    default:
+      return state;
+  };
+}
 
 export const store = configureStore({
   reducer: rootReducer,
