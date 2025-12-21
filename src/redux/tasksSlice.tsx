1ac2f1ff6f53ec46264dbb42e907cba28632f551
kostyaslob <kostyaslob@gmail.com>
2025-12-21 22:53:31 +0200

    refactor: update taskSlice  via createSlice()

--- src/redux/tasksSlice.tsx
+++ src/redux/tasksSlice.tsx
@@ -1,12 +1,9 @@
-import { createAction } from "@reduxjs/toolkit";
-import type { RootState } from "./store";
+import { createSlice } from "@reduxjs/toolkit";
+// import type { RootState } from "./store";
 
-export const addTask = createAction("tasks/addTask");
-export const deleteTask = createAction("tasks/deleteTask");
-export const toggleCompleted = createAction("tasks/toggleCompleted");
-
-const initialState: RootState = {
-  tasks: {
+const slice = createSlice({
+  name: "tasks",
+  initialState: {
     items: [
       { id: 0, text: "Learn HTML and CSS", completed: true },
       { id: 1, text: "Get good at JavaScript", completed: true },
@@ -14,44 +11,45 @@ const initialState: RootState = {
       { id: 3, text: "Discover Redux", completed: false },
       { id: 4, text: "Build amazing apps", completed: false },
     ],
-  },
-};
-
-export default function tasksReducer (state: RootState = initialState.tasks, action: { type: string }): RootState {
-  switch (action.type) {
-    case "tasks/addTask": {
-      return {
-        ...state,
-        tasks: {
-          items: [...state.tasks.items, action.payload]
-        }
-      }
-    }
-    case "tasks/deleteTask": {
-      return {
-        ...state,
-        tasks: {
-          items: state.tasks.items.filter((task) => task.id !== action.payload)
-        }
-      }
-    }
-    case "tasks/toggleCompleted": {
-      return {
-        ...state,
-        tasks: {
-          items: state.tasks.items.map((task) => {
-            if (task.id !== action.payload) {
-              return task
-            }
+    },
+    reducers: {
+        addTask: (state, action) => {
             return {
-              ...task,
-              completed: !task.completed
-            }
-          })
+              ...state,
+              tasks: {
+                items: [...state.tasks.items, action.payload],
+              },
+            };
+        },
+        deleteTask: (state, action) => {
+            return {
+              ...state,
+              tasks: {
+                items: state.tasks.items.filter(
+                  (task) => task.id !== action.payload
+                ),
+              },
+            };
+        },
+        toggleCompleted: (state, action) => {
+            return {
+              ...state,
+              tasks: {
+                items: state.tasks.items.map((task) => {
+                  if (task.id !== action.payload) {
+                    return task;
+                  }
+                  return {
+                    ...task,
+                    completed: !task.completed,
+                  };
+                }),
+              },
+            };
         }
-      }
-    }  
-    default:
-      return state;
-  };
-}
+   }
+});
+
+export const { addTask, deleteTask, toggleCompleted } = slice.actions;
+
+export default slice.reducer;
