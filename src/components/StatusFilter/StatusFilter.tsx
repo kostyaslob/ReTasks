7e9c667402f64ab7ad81b0d50d72eb6cb004cc10
kostyaslob <kostyaslob@gmail.com>
2025-12-26 20:28:15 +0200

    fix: update StatusFilter component

--- src/components/StatusFilter/StatusFilter.tsx
+++ src/components/StatusFilter/StatusFilter.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Button from "../Button/Button";
 import css from "./StatusFilter.module.css";
 import type { RootState } from "../../redux/store";
-import { setStatusFilter } from "../../redux/actions";
+import { setStatusFilter } from "../../redux/filtersSlice";
 
 export default function StatusFilter() {
   const dispatch = useDispatch();
