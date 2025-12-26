e3152c0202cdc868b04c5a85513ff79396f1c8b5
kostyaslob <kostyaslob@gmail.com>
2025-12-26 20:16:23 +0200

    refactor: pending and rejected reducers were moved into separate functions

--- src/redux/tasksSlice.ts
+++ src/redux/tasksSlice.ts
@@ -2,6 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 import { addTask, deleteTask, fetchTasks, toggleCompleted } from "./operations";
 // import type { RootState } from "./store";
 
+const handlePending = (state) => {
+  state.isLoading = false
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload
+}
+
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
@@ -11,46 +20,31 @@ const tasksSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(fetchTasks.pending, (state) => {
-        state.isLoading = true;
-      })
+      .addCase(fetchTasks.pending, handlePending)
       .addCase(fetchTasks.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items = action.payload;
       })
-      .addCase(fetchTasks.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload
-      })
-      .addCase(addTask.pending, (state) => {
-        state.isLoading = true;
-    }).addCase(addTask.fulfilled, (state, action) => {
+      .addCase(fetchTasks.rejected, handleRejected)
+      .addCase(addTask.pending, handlePending)
+      .addCase(addTask.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items.push(action.payload);
-    }).addCase(addTask.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    }).addCase(deleteTask.pending, (state) => {
-        state.isLoading = true;
-    }).addCase(deleteTask.fulfilled, (state, action) => {
+      }).addCase(addTask.rejected, handleRejected)
+      .addCase(deleteTask.pending, handlePending)
+      .addCase(deleteTask.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items = state.items.filter((task) => task.id !== action.payload.id)
-    }).addCase(deleteTask.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    }).addCase(toggleCompleted.pending, (state) => {
-        state.isLoading = true;
-    }).addCase(toggleCompleted.fulfilled, (state, action) => {
+      }).addCase(deleteTask.rejected, handleRejected)
+      .addCase(toggleCompleted.pending, handlePending)
+      .addCase(toggleCompleted.fulfilled, (state, action) => {
         state.isLoading = false;
       state.error = null;
       state.items = state.items.map((task) => task.id === action.payload.id ? action.payload : task)
-    }).addCase(toggleCompleted.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-    })
+    }).addCase(toggleCompleted.rejected, handleRejected)
   },
 });
 
