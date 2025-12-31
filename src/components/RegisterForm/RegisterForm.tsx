4a9af83197b49b59b078c8ceb0b7171203ab382b
kostyaslob <kostyaslob@gmail.com>
2025-12-31 19:26:54 +0200

    feat: add RegisterForm component

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,5 +1,35 @@
-// import css from "./RegisterForm.module.css";
+import css from "./RegisterForm.module.css";
+
+import { Formik, Form, Field } from "formik";
 
 export default function RegisterForm() {
-    return <p>RegisterForm</p>;
+    const handleSubmit = (event) => {
+
+    }
+
+    const initialValues = {
+        name: "",
+        email: "",
+        password: ""
+    }
+
+    return (
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Form className={css.form}>
+                <label className={css.label}>
+                    Username
+                    <Field type="text" name="name"/>
+                </label>
+                <label className={css.label}>
+                    Email
+                    <Field type="text" name="email"/>
+                </label>
+                <label className={css.label}>
+                    Password
+                    <Field type="text" name="password"/>
+                </label>
+                <button type="submit">Register</button>
+            </Form>
+        </Formik>
+    )
 }
