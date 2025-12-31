b716fe7d6d5a03ac98a5392e6d17d4e72a94c48c
kostyaslob <kostyaslob@gmail.com>
2025-12-31 19:40:58 +0200

    feat: add conditional rendering for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,17 @@ import css from "./Header.module.css";
 import Navigation from "../Navigation/Navigation";  
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export default function Header() {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
-        <header className={css.header}>
-            <Navigation />
-            <UserMenu />
-            <AuthNav />
-        </header>
-    )
+      <header className={css.header}>
+        <Navigation />
+        {isLoggedIn && <UserMenu />}
+        <AuthNav />
+      </header>
+    );
 }
