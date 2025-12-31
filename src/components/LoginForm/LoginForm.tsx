ed4ae90bcb591b0ec8592a33bb1932df1fd1eee2
kostyaslob <kostyaslob@gmail.com>
2025-12-31 19:30:35 +0200

    feat: add LoginForm component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,28 @@
-// import css from "./LoginForm.module.css";
+import css from "./LoginForm.module.css";
+import { Formik, Form, Field } from "formik";
 
 export default function LoginForm() {
-  return <p>LoginForm</p>;
+  const handleSubmit = (event) => {};
+
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className={css.form}>
+        <label className={css.label}>
+          Email
+          <Field type="text" name="email" />
+        </label>
+        <label className={css.label}>
+          Password
+          <Field type="text" name="password" />
+        </label>
+        <button type="submit">Log In</button>
+      </Form>
+    </Formik>
+  );
 }
