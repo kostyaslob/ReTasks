fdddbfc9c672e4b68cfd8cdd0b7bd72c95f10537
kostyaslob <kostyaslob@gmail.com>
2026-01-04 19:44:56 +0200

    feat: add conditional rendering for Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,16 +1,22 @@
+import { useSelector } from "react-redux";
 import css from "./Navigation.module.css"
 
 import { NavLink } from "react-router-dom";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export default function Navigation() {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
       <nav className={css.wrapper}>
         <NavLink className={css.link} to="/">
           Home
         </NavLink>
-        <NavLink className={css.link} to="/tasks">
-          Tasks
-        </NavLink>
+        {isLoggedIn && (
+          <NavLink className={css.link} to="/tasks">
+            Tasks
+          </NavLink>
+        )}
       </nav>
     );
 }
