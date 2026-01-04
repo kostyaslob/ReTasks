bc23f9a0e955b1e4d6eff756312c72055017e115
kostyaslob <kostyaslob@gmail.com>
2026-01-04 19:39:23 +0200

    refactor: add setAuthHeader for Bearer.token

--- src/redux/auth/operations.ts
+++ src/redux/auth/operations.ts
@@ -3,19 +3,23 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://task-manager-api.goit.global/";
 
+const setAuthHeader = value => {
+    axios.defaults.headers.common.Authorization = value;
+}
+
 export const register = createAsyncThunk("auth/register", async (values) => {
     const response = await axios.post("/users/signup", values);
-    axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`
+    setAuthHeader(`Bearer ${response.data.token}`)
     return response.data
 })
 
 export const logIn = createAsyncThunk("auth/login", async (values) => {
     const response = await axios.post("/users/login", values);
-    axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`
+    setAuthHeader(`Bearer ${response.data.token}`)
     return response.data
 })
 
 export const logOut = createAsyncThunk("auth/logout", async () => {
     await axios.post("/users/logout");
-    axios.defaults.headers.common.Authorization = "";
+    setAuthHeader("");
 })
