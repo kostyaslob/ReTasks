2dca5bfea1e85b05a0e3207d2495de443ba28206
kostyaslob <kostyaslob@gmail.com>
2026-01-07 21:51:07 +0200

    feat: add token saving to the local storage via redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,14 +3,39 @@ import tasksReducer from "./tasksSlice";
 import filtersReducer from "./filtersSlice";
 import authReducer from "./auth/authSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
     filters: filtersReducer,
-    auth: authReducer,
-  }
+    auth: persistReducer(authPersistConfig, authReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
